581e1281129c21ce3b974016a7c9ff5a543c1e23
Anastasia <anastasia.farid@gmail.com>
2024-06-10 08:54:27 +0300

    front(footer and navbar)

--- Front/paneli/Navbar.js
+++ Front/paneli/Navbar.js
@@ -1,25 +1,22 @@
 // Navbar.js
 $(document).ready(function() {
     var navbar = `
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">StarostaOnline</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Студенты</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Группы</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/Front/img/логотип.png">Starosta.online</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
     `;
     $('body').prepend(navbar); // Add the navbar to the top of the body
 });
