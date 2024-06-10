e68a21eda63c68a7085495a2550e4c9a34565082
Anastasia <anastasia.farid@gmail.com>
2024-06-10 13:08:13 +0300

    navbar work and app.js update logical

--- Front/App.js
+++ Front/App.js
@@ -3,13 +3,35 @@ $(document).ready(function() {
     $.getScript("Front/paneli/navbar.js", function() {
         // Скрипт загружен и выполнен
     });
-    // Подключаем файл frontend.js
-    $.getScript("Front/components/crud_students.js", function() {
-        // Скрипт загружен и выполнен
+
+    // Handle click event for "Добавление студента" link
+    $(document).on('click', '#add-student-link', function(e) {
+        e.preventDefault();
+        // Load and execute the crud_students.js script
+        $.getScript("Front/components/crud_students.js", function() {
+            // Скрипт загружен и выполнен
+        });
     });
-    
+
     // Подключаем файл footer.js
     $.getScript("Front/paneli/footer.js", function() {
         // Скрипт загружен и выполнен
     });
+    // Подключаем файл auth.js
+    $.getScript("Front/components/auth.js", function() {
+        // Скрипт загружен и выполнен
+    });
+
+    
+
+
+    // Handle click event for "Добавление студента" link
+    $(document).on('click', '#autorization-link', function(e) {
+        e.preventDefault();
+        // Load and execute the crud_students.js script
+        $.getScript("Front/components/auth.js", function() {
+            // Скрипт загружен и выполнен
+        });
+    });
+
 });
--- Front/paneli/Navbar.js
+++ Front/paneli/Navbar.js
@@ -1,40 +1,36 @@
-// Navbar.js
 $(document).ready(function() {
-    var navbar = `
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="logo.png"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Добавление
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Добавление студента</a></li>
-            <li><a class="dropdown-item" href="#">Добавление преподавателя</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Добавление предмета</a></li>
-          </ul>
-        </li>
-       
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    `;
-    $('body').prepend(navbar); // Add the navbar to the top of the body
+  var navbar = `
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="logo.png"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              Добавление
+                          </a>
+                          <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" id="add-student-link">Добавление студента</a></li>
+                              <li><a class="dropdown-item">Добавление преподавателя</a></li>
+                              <li><a class="dropdown-item">Добавление предмета</a></li>
+                          </ul>
+                      </li>
+                  </ul>
+                  <form class="d-flex" role="search">
+                      <button class="btn btn-outline-success" type="submit" id="autorization-link">Вход</button>
+                  </form>
+              </div>
+          </div>
+      </nav>
+  `;
+  $('body').prepend(navbar);
 });
