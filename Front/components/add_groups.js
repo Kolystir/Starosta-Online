1eac08922825a325ed49dcfce6380836c3667701
Kolystir <57070191+Kolystir@users.noreply.github.com>
2024-07-01 15:33:52 +0300

    удаление группы запрещенно, и также с предметами, а также нумерация появилась

--- Front/components/add_groups.js
+++ Front/components/add_groups.js
@@ -5,39 +5,22 @@ $(document).ready(function () {
             content += "<div class='card-body'>";
             content += "<table class='table table-striped fixed-table' style='width: 100%;'>";
             content += "<thead><tr>";
-            content += "<th class='id-col'>ID</th>";
+            content += "<th class='num-col'>№</th>";
             content += "<th class='name-col'>Название группы</th>";
             content += "<th class='edit-col'>Редактирование</th>";
-            content += "<th class='delete-col'>Удаление</th>";
             content += "</tr></thead><tbody>";
-            data.forEach(function (group) {
+
+            data.forEach(function (group, index) {
                 content += "<tr>";
-                content += "<td class='id-col'>" + group.Group_ID + "</td>";
+                content += "<td class='num-col'>" + (index + 1) + "</td>";
                 content += "<td class='name-col'>" + group.Group_Name + "</td>";
-                content += '<td class="edit-col"><button class="btn btn-secondary edit-btn" data-id="' + group.Group_ID + '" data-name="' + group.Group_Name + '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/></svg></button></td>';
-                content += '<td class="delete-col"><button class="btn btn-danger delete-btn" data-id="' + group.Group_ID + '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/></svg></button></td>';
+                content += '<td class="edit-col"><button class="btn btn-secondary edit-btn" data-id="' + group.Group_ID + '" data-name="' + group.Group_Name + '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293z"/></svg></button></td>';
                 content += "</tr>";
             });
+
             content += "</tbody></table>";
             content += "</div></div>";
             $("#groups-table").html(content);
-
-            $(".delete-btn").click(function () {
-                var groupId = $(this).data("id");
-                if (confirm("Вы уверены что хотите удалить эту группу?")) {
-                    $.ajax({
-                        url: "http://localhost:8000/groups/" + groupId,
-                        type: "DELETE",
-                        success: function () {
-                            loadGroups();
-                            alert("Группа успешно удалена!");
-                        },
-                        error: function () {
-                            alert("Произошла ошибка при удалении группы.");
-                        },
-                    });
-                }
-            });
 
             $(".edit-btn").click(function () {
                 var groupId = $(this).data("id");
@@ -104,8 +87,6 @@ $(document).ready(function () {
             },
         });
     }
-            
-
 
     var app = $("#app");
     var content = `
@@ -126,7 +107,7 @@ $(document).ready(function () {
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="editModalLabel">Редактирование группы</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -140,7 +121,7 @@ $(document).ready(function () {
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
                 <button type="button" class="btn btn-primary" id="saveBtn">Сохранить изменения</button>
             </div>
         </div>
@@ -155,4 +136,4 @@ $(document).ready(function () {
     $("#saveBtn").on("click", updateGroup); // обработка сохранения в модальном окне
 
     loadGroups();
-}); 
+});
